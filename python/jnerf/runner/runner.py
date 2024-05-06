1dcabac005f0dd155fec805fb63e71661bc7e0d9
yindaheng98 <yindaheng98@gmail.com>
2024-05-06 22:55:08 +0000

    basic scaffold

--- python/jnerf/runner/runner.py
+++ python/jnerf/runner/runner.py
@@ -4,9 +4,8 @@ from PIL import Image
 import numpy as np
 from tqdm import tqdm
 from jnerf.ops.code_ops import *
-from jnerf.dataset.dataset import jt_srgb_to_linear, jt_linear_to_srgb
-from jnerf.utils.config import get_cfg, save_cfg
-from jnerf.utils.registry import build_from_cfg, NETWORKS, SCHEDULERS, DATASETS, OPTIMS, SAMPLERS, LOSSES
+from jnerf.utils.config import get_cfg
+from jnerf.utils.registry import build_from_cfg, NETWORKS, DATASETS, OPTIMS, SAMPLERS, LOSSES
 from jnerf.models.losses.mse_loss import img2mse, mse2psnr
 from jnerf.dataset import camera_path
 import cv2
